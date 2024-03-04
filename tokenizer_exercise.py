de772674683878c38ec28dbb3c98b68ccb3eba4a
Neda <neda.sultova@gmail.com>
2024-03-04 19:04:29 +0100

    basic refactoring

--- tokenizer_exercise.py
+++ tokenizer_exercise.py
@@ -1,4 +1,11 @@
 
+"""
+Exercise 01 as found in
+https://github.com/karpathy/minbpe/blob/master/exercise.md
+Most basic PoC of BPE for my own understanding
+https://en.wikipedia.org/wiki/Byte_pair_encoding
+"""
+
 def count_bigrams(text):
     bigram_occurence = {}
     for i in range(len(text) -1):
@@ -8,7 +15,7 @@ def count_bigrams(text):
         bigram_occurence[bigram] += 1
     return bigram_occurence
 
-def find_max_occurence(text):
+def most_frequent_bigram(text):
     max_count = 0
     max_bigram = None
     for bigram, count in count_bigrams(text).items():
@@ -20,34 +27,39 @@ def find_max_occurence(text):
     return max_bigram
 
 class DummyTokenizer():
-
     def __init__(self, vocab_size=256, num_merges=3):
         self.vocab_size = vocab_size
         self.merges = num_merges
         self.vocab_map = dict([(i,chr(i)) for i in range(256)])
 
     def _find_next_vocab_idx(self):
+        """
+        Find the next free spot in vocab_map for inserting a non_terminal symbol mapping 
+        to the bigram in question
+        """
         return len(self.vocab_map)
     
     def _non_terminal_symbol(self, vocab_map_idx):
+        """
+        In our dummy case non-terminal symbols are [Z-A], each denoting a bigram to be substituted
+        """
         symbol_idx = vocab_map_idx - self.vocab_size
         ASCII_Z = 90
         return chr(ASCII_Z - symbol_idx) 
 
-    def merge(self, text):
-        max_bigram = find_max_occurence(text)
+    def _merge(self, text):
+        bigram = most_frequent_bigram(text)
         next_substitution = self._non_terminal_symbol(self._find_next_vocab_idx())
-        text = text.replace(max_bigram, next_substitution)
-        self.vocab_map[self._find_next_vocab_idx()] = max_bigram
+        text = text.replace(bigram, next_substitution)
+        self.vocab_map[self._find_next_vocab_idx()] = bigram
         return text
 
     def train(self, text):
         for i in range(self.merges):
-            text = self.merge(text)
+            text = self._merge(text)
         return text 
 
     def encode(self, text):
-        #return [258, 100, 258, 97, 99]
         for (idx, bigram ) in list(self.vocab_map.items())[256:]:
         #print(idx,bigram, non_terminal_symbol(idx))
             text = text.replace(bigram, self._non_terminal_symbol(idx))
@@ -66,7 +78,7 @@ def test():
     #print(tokenizer.vocab_map)
     #print(tokenizer._find_next_vocab_idx())
     #print(tokenizer._non_terminal_symbol(257))
-    print(tokenizer.merge("xyzxyzxyz"))
+    print(tokenizer._merge("xyzxyzxyz"))
     text = "aaabdaaabac"
     tokenizer.train(text) # 256 are the byte tokens, then do 3 merges
     print(tokenizer.encode(text))
